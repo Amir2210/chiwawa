5ee461a5f463b4f6dd23361ad7ea85ed4607bfc3
Amir2210 <amiryankolovich2000@gmail.com>
2025-07-02 11:38:28 +0300

    Connet app context to the app

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContextnp, useState } from "react";
 
 const GlobalContext = createContext();
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { AppContext } from "./AppContext";
+
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
+  <AppContext>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </AppContext>
+);
