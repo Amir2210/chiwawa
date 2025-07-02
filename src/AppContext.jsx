4279023e3c2d98a06b21b3246f6da66314900da1
Amir2210 <amiryankolovich2000@gmail.com>
2025-07-02 12:10:08 +0300

    Add some demo data locations

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,18 +1,51 @@
 import { createContext, useContext, useState } from "react";
 
-const GlobalContext = createContext()
+const GlobalContext = createContext();
 
 export const useGlobalContext = () => {
-  return useContext(GlobalContext)
-}
+  return useContext(GlobalContext);
+};
 
 export function AppContext({ children }) {
-  const [searchInput, setSearchInput] = useState("")
+  const [searchInput, setSearchInput] = useState("");
+  const [locations, setLocations] = useState([
+    {
+      id: 1,
+      Latitude: 56.1721987,
+      Longitude: 93.6466243,
+      Phone: "+7 923 123-45-67",
+      StoreName: "padbeeg, thiel and lubowitz",
+      ZipCode: "12-3456",
+    },
+    {
+      id: 2,
+      Latitude: 53.1620102,
+      Longitude: 16.6118082,
+      Phone: "345-678-9012",
+      StoreName: "feil and sons",
+      ZipCode: "34-5678",
+    },
+    {
+      id: 3,
+      Latitude: 52.1620102,
+      Longitude: 16.6118082,
+      Phone: "+7 563 123-45-67",
+      StoreName: "hoewer, feeney and moore",
+      ZipCode: "66-7890",
+    },
+    {
+      id: 4,
+      Latitude: 24.7762658,
+      Longitude: -107.4046797,
+      Phone: "+52 123 456-7890",
+      StoreName: "walter, moen and mohr",
+      ZipCode: "33-4567",
+    },
+  ]);
 
   function onSearchStore(input) {
     const newSearchInput = input;
-    setSearchInput(newSearchInput)
-
+    setSearchInput(newSearchInput);
   }
   return (
     <GlobalContext.Provider value={{ onSearchStore, searchInput }}>
